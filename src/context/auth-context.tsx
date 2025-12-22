aa3a13f4959ce9a2f52c090ec675779366bdfe01
cybercircusconnects <jawadprogrammer@gmail.com>
2025-12-23 02:20:58 +0500

    change

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -182,8 +182,10 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
 export function useAuth() {
   const context = useContext(AuthContext);
+
   if (context === undefined) {
     throw new Error("useAuth must be used within an AuthProvider");
   }
+  
   return context;
 }
